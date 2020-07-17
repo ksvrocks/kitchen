f06988355eb539c5e86984f45eeb29a6b2d6527a
ksvrocks <ksvrocks@gmail.com>
2020-07-17 18:31:01 +0530

    comment index

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ include_once 'temp-part/section1.php';
 
 include_once 'temp-part/section2.php';
 
-include_once 'temp-part/workingProcess.php';
+// include_once 'temp-part/workingProcess.php';
 
-include_once 'temp-part/productSlider.php';
+// include_once 'temp-part/productSlider.php';
 
 include_once 'footer.php';
 
